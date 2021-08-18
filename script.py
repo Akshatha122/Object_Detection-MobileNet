7fbae5a965fc10dc2fab0628a2dffa1a05a76038
Akshatha Mohan <52374337+Akshatha122@users.noreply.github.com>
2021-08-18 13:19:09 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,18 @@
+ffmpeg = open("infbuf_mpeg_filtered.log",'r') #ffmpeg side log-file (filtered)
+ffplay = open("infbuf_play_filtered.log",'r') #ffplay side log-file (filtered)
+fout = open("buf_infbuf.plot",'w') #output file.
+arr1 = [[int(x) for x in line.split()] for line in ffmpeg]
+arr2 = [[int(x) for x in line.split()] for line in ffplay]
+arr=[]
+for i in range(len(arr2)):
+    if( arr2[i][0] - arr1[i][0] > 10 ):  # We are using packet size to identify packet at both side
+        print("Packet size not maching at line:",i+1)
+    arr.append(arr2[i][1]-arr1[i][1])
+arr.sort()
+
+for a in arr:
+    fout.write(str(a)+"\n")
+ffmpeg.close()
+ffplay.close()
+fout.close()
+print("\t\t\t ||Done||")
